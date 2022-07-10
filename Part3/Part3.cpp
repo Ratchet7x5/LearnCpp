e4e3c59db99d48056329aeee49400f6a9b65f519
Tarun <36789694+Ratchet7x5@users.noreply.github.com>
2022-07-10 12:40:15 +0530

    update print statements in main()

--- Part3/Part3.cpp
+++ Part3/Part3.cpp
@@ -14,5 +14,14 @@ struct cow
 
 int main()
 {
-	std::cout << "Hello World!\n";
+	//the cow struct 
+	cow my_cow;
+
+	//init the values
+	my_cow.name = "Betty";
+	my_cow.age = 10;
+	my_cow.purpose = cow_purpose::dairy;
+
+	std::cout << "The cow's name is \"" << my_cow.name << "\" and its " << my_cow.age << " years old.\n";
+	std::cout << my_cow.name << "'s purpose is type-" << (int)my_cow.purpose; //int-cast to see "type-X"
 }
