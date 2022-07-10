8cce778648a1e7ed7f7a44db8e3be974658930ec
Tarun <36789694+Ratchet7x5@users.noreply.github.com>
2022-07-10 12:59:01 +0530

    implement setters

--- Part3/Part3.cpp
+++ Part3/Part3.cpp
@@ -1,29 +1,57 @@
 #include <iostream>
+#include <string>
 
 enum cow_purpose
 {
 	dairy, meat, hide, pet
 };
 
-struct cow
+class cow
 {
-	std::string name = "-1";
-	int age = -1;
-	unsigned char purpose = 0;
+public:
+	cow(std::string in_name, int in_age, unsigned char in_purpose) {
+		name = in_name;
+		age = in_age;
+		purpose = in_purpose;
+	};
+
+	int get_age() {
+		return age;
+	};
+
+	std::string get_name() {
+		return name;
+	};
+
+	unsigned char get_purpose() {
+		return purpose;
+	};
+	
+	//setters
+	void set_age(int new_age) {
+		age = new_age;
+	};
+
+	void set_name(std::string new_name) {
+		name = new_name;
+	};
+
+	void set_purpose(unsigned char new_purpose) {
+		purpose = new_purpose;
+	};
+private: //getter setters req'd
+	std::string name;
+	int age;
+	unsigned char purpose;
 };
 
 int main()
 {
 	//the cow struct 
-	cow my_cow;
+	cow my_cow("Hilda", 7, 'P');
 
-	//init the values
-	my_cow.name = "Betty";
-	my_cow.age = 10;
-	my_cow.purpose = cow_purpose::dairy;
-
-	std::cout << "The cow's name is \"" << my_cow.name << "\" and its " << my_cow.age << " years old.\n";
-	std::cout << my_cow.name << "'s purpose is type-" << (int)my_cow.purpose; //int-cast to see "type-X"
+	std::cout << "The cow's name is \"" << my_cow.get_name() << "\" and its " << my_cow.get_age() << " years old.\n";
+	std::cout << my_cow.get_name() << "'s purpose is type-" << (int)my_cow.get_purpose() << std::endl; //int-cast to see "type-X"
 
 	return 0;
 }
